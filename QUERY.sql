8435e602eb4152d8a898f52eac9320d06fbc542b
zubaer-rahman <zubaer.rahman.cse@gmail.com>
2026-06-14 08:59:50 +0600

    Add query to retrieve upcoming Champions League matches

--- QUERY.sql
+++ QUERY.sql
@@ -76,3 +76,11 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+-- =========================================================================
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'
+-- =========================================================================
+SELECT match_id, fixture, base_ticket_price FROM matches 
+  WHERE tournament_category = 'Champions League' AND match_status = 'Available'
+
+
